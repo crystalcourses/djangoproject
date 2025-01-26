412d45b73bdd00bec5531f7eddc421618c56ee3b
crystalcourses <rinlafleur@icloud.com>
2025-01-26 23:50:15 +0300

    add: register, profile, login views created, bug fixed

--- crystalcourses/urls.py
+++ crystalcourses/urls.py
@@ -19,15 +19,15 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('cart/', include('cart.urls', namespace='cart')),
-    path('orders/', include('orders.urls', namespace='orders')),
     path('user/', include('users.urls', namespace='user')),
+    path('orders/', include('orders.urls', namespace='orders')),
     path('', include('main.urls', namespace='main')),
 ]
 
-
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
-                          document_root = settings.MEDIA_ROOT)
+                          document_root=settings.MEDIA_ROOT)
--- users/forms.py
+++ users/forms.py
@@ -7,12 +7,48 @@ from .models import User
 class UserLoginForm(AuthenticationForm):
     username = forms.CharField()
     password = forms.CharField()
-
-
+    
+    
     class Meta:
         model = User
         fields = ['username', 'password']
-
+        
 
 class UserRegistrationForm(UserCreationForm):
     class Meta:
+        model = User
+        fields = (
+            'first_name',
+            'last_name',
+            'username',
+            'email',
+            'password1',
+            'password2',
+        )
+        
+        
+        first_name = forms.CharField()
+        last_name = forms.CharField()
+        username = forms.CharField()
+        email = forms.CharField()
+        password1 = forms.CharField()
+        password2 = forms.CharField()
+        
+
+class ProfileForm(UserChangeForm):
+    class Meta:
+        model = User
+        fields = (
+            'image',
+            'first_name',
+            'last_name',
+            'username',
+            'email',
+        )
+    
+    
+        image = forms.ImageField(required=False)
+        first_name = forms.CharField()
+        last_name = forms.CharField()
+        username = forms.CharField()
+        email = forms.CharField()
