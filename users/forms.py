63f681beb5b749ffc14c0c8faaff8bc69644574b
crystalcourses <rinlafleur@icloud.com>
2025-01-26 19:38:44 +0300

    added: create order, inputs in create order. if inputs=fill then created.html, orders in db. login, registration

--- users/forms.py
+++ users/forms.py
@@ -1,5 +1,6 @@
 from django import forms
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, \
+    UserCreationForm, UserChangeForm
 from .models import User
 
 
@@ -11,3 +12,7 @@ class UserLoginForm(AuthenticationForm):
     class Meta:
         model = User
         fields = ['username', 'password']
+
+
+class UserRegistrationForm(UserCreationForm):
+    class Meta:
